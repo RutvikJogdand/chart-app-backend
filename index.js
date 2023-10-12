6e045ab916d2124f1a345f42e6fd7709492d4d45
RutvikJogdand <jogdandrutvik7@gmail.com>
2023-10-13 00:50:06 +0530

    made a nodejs app to scan a front-end route and generate pdf

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const cors = require("cors")
+
+const app = express();
+app.use(cors())
+const router = express.Router()
+const port = 3001;
+
+router.get('/generate-pdf', async (req, res) => {
+  const browser = await puppeteer.launch({
+    headless: 'new',
+  });
+  const page = await browser.newPage();
+  
+  await page.goto('http://localhost:3000/arrests-data');
+  
+  await page.waitForSelector("svg");
+  await page.emulateMediaType('screen')
+
+  await page.evaluate(async() => {
+    await new Promise(function(resolve) { 
+           setTimeout(resolve, 4000)
+    });
+});
+
+  // Capture a screenshot of the chart
+  const screenshotBuffer = await page.screenshot();
+
+  // Convert the screenshot to a PDF
+  const pdfBuffer = await page.pdf({ format: 'A4' });
+  
+  res.set('Content-Type', 'application/pdf');
+  res.send(pdfBuffer);
+
+  await browser.close();
+});
+
+app.use("/api", router)
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
